8fc3cc2caeceb0d5a34d62c68c115fa68a1bc3bb
Dat Nguyen <ngdangdat09@gmail.com>
2017-11-07 23:22:42 +0700

    Modify README.md

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
+var gulpIf = require('gulp-if');
 var sass = require('gulp-sass');
 var cssMinify = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 const PATHS = {
 	CSS: './static/css'
@@ -10,13 +13,17 @@ const PATHS = {
 
 gulp.task('css', function() {
 	gulp.src('sass/**/*.scss')
+		.pipe(gulpIf(gulpUtil.env.sourcemaps, sourcemaps.init()))
 		.pipe(sass().on('error', sass.logError))
+		.pipe(gulpIf(gulpUtil.env.sourcemaps, sourcemaps.write('./')))
 		.pipe(cssMinify({compatibility: 'ie8', processImport: false}))
 		.pipe(gulp.dest(PATHS.CSS));
 });
 
 gulp.task('build', ['css']);
 
-gulp.task('default', function() {
+gulp.task('watch', function() {
 	gulp.watch('sass/**/*.scss', ['css']);
-});
+})
+
+gulp.task('default', ['watch']);
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# vgujobboard
+# VGU Job Boards
+
+## Set up environment
+
+To set up environment, make sure you have npm. You can download npm here: https://www.npmjs.com/
+
+Next, install gulp and gulp dependencies by run following commands in command line:
+
+
+```
+npm install gulp -g
+npm install
+
+```
+
+
+## Build commands
+
+After setting up environment, you can start write SCSS in *sass* folder.
+
+Build and watch commands:
+
+```
+gulp build
+gulp watch
+```
+
+Add *--sourcemaps* as parameter of commands above to generate SCSS and CSS map.
+
+Feel free to ask me any question at ngdangdat09 [at] gmail.com.
