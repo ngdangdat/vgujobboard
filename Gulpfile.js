e6d3e53a06690e1d2adea40886d466ca91861a42
Dat Nguyen <ngdangdat09@gmail.com>
2017-11-28 23:16:19 +0700

    make it better

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,7 +16,6 @@ gulp.task('css', function() {
 		.pipe(gulpIf(gulpUtil.env.sourcemaps, sourcemaps.init()))
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulpIf(gulpUtil.env.sourcemaps, sourcemaps.write('./')))
-		.pipe(cssMinify({compatibility: 'ie8', processImport: false}))
 		.pipe(gulp.dest(PATHS.CSS));
 });
 
