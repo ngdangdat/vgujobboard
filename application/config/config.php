bb62b53f2e9abd818a2be7d0065e9812e4c2dfb1
Dat Nguyen <ngdangdat09@gmail.com>
2017-11-29 23:24:48 +0700

    Edit static helper

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/intern';
+$config['base_url'] = 'http://localhost/intern';
 
 /*
 |--------------------------------------------------------------------------
--- application/helpers/static_helper.php
+++ application/helpers/static_helper.php
@@ -5,6 +5,7 @@ if(!function_exists('getStaticPath')) {
      * Get absolute static path for given path
      */
     function getStaticPath($path) {
-        return (get_instance()->config->base_url('', getCurrentProtocol()) . get_instance()->config->static_path() . '/' . $path);
+
+        return (get_instance()->config->site_url(get_instance()->config->static_path() . '/' . $path));
     }
 }
