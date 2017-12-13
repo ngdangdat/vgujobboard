251514c309f1496c91f6dedc45c25cee6bb256e7
Dat Nguyen <ngdangdat09@gmail.com>
2017-12-14 01:54:00 +0700

    Hotfix

--- application/views/component/htmlhead.php
+++ application/views/component/htmlhead.php
@@ -1,3 +1,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="shortcut icon" href="<?php echo getStaticPath('images/favicon.ico'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo getStaticPath('css/style.css'); ?>" />
