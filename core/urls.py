7788bfb4411968199d5221be1348b721291de6a0
Hung Dong <nhuthung.dong@gmail.com>
2018-04-30 23:37:54 +0700

    Support Signup function + Simple signup view
    Signed-off-by: Hung Dong <nhuthung.dong@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin_site.urls),
-    path('', include('frontsite.route'))
+    path('', include('frontsite.route')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
