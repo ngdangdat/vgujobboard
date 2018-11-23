3b504991c2877927152adfdcd66d19ef22745411
blackbird <ngdangdat09@gmail.com>
2018-11-24 00:20:42 +0700

    Add user status to list display in CMS

--- src/admin/admin/user.py
+++ src/admin/admin/user.py
@@ -38,7 +38,7 @@ class UserProfileInline(admin.StackedInline):
 
 class UserAdmin(DjangoUserAdmin):
   inlines = [UserProfileInline]
-  list_display = ('email', 'first_name', 'last_name', 'is_staff')
+  list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
   list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
   search_fields = ('email', 'first_name', 'last_name')
   ordering = ('id', )
